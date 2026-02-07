780a6d271326b4cb2ce3965beea721f9e48eed1e
Shouri Chakraborty <107758587+shouri123@users.noreply.github.com>
2026-02-07 11:55:23 +0530

    Hello

--- array.java
+++ array.java
@@ -19,7 +19,22 @@ class array {
             System.out.println(arr[i]);
         }
         Sc.close();
+        bubbleSort(arr,n);
     }
 
-    
+    public static void bubbleSort(int[] arr,int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n-i-1;j++){
+                if (arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("\n The Nos After SortingAre \n");
+        for (int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }           
+    }
 }
